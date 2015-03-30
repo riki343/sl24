4565bc5eb44885fb90cccb5a14fcb6e424cb6f13
riki343 <riki34@spaces.ru>
2015-03-30 16:35:01 +0300

    First commit...

---
+++ src/Sl24Bundle/Entity/WorkingMonth.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sl24Bundle\Entity;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Sl24Bundle\Entity\User;
+
+/**
+ * WorkingMonth
+ *
+ * @ORM\Table(name="working_months")
+ * @ORM\Entity
+ */
+class WorkingMonth
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    private $startDate;
+    private $endDate;
+
+    private $user;
+}
