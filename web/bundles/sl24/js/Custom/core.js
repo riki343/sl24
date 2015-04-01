e26c9dc3cde6052f0fb9e90401fad8435ca55cf3
riki343 <riki34@spaces.ru>
2015-04-01 12:30:33 +0300

    Database + design

--- web/bundles/sl24/js/Custom/core.js
+++ web/bundles/sl24/js/Custom/core.js
@@ -1,9 +1,23 @@
 var Sl24 = angular.module('Sl24', ['ngRoute', 'ngAnimate']);
 
-Sl24.config(['$interpolateProvider', '$httpProvider',
-    function ($interpolateProvider, $httpProvider) {
+Sl24.config(['$interpolateProvider', '$httpProvider', '$routeProvider', '$locationProvider',
+    function ($interpolateProvider, $httpProvider, $routeProvider, $locationProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
         $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+        $routeProvider
+            .when('/', {
+                templateUrl: TEMPLATES.index,
+                controller: 'indexController'
+            })
+            .when('/trading', {
+                templateUrl: TEMPLATES.trading,
+                controller: 'tradingController'
+            })
+            .when('/logout', {
+            });
+
+        $locationProvider.html5Mode(true);
     }
 ]);
