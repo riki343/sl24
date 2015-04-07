fedd865f5ab80078adb85847a3889d4a73c662ba
PilliVilli <andriismuzhanitsya@gmail.com>
2015-04-07 14:48:45 +0200

    teams

--- web/bundles/sl24/js/Custom/teamController.js
+++ web/bundles/sl24/js/Custom/teamController.js
@@ -1,12 +1,40 @@
 Sl24.controller('TeamController', ['$scope', '$http',
     function ($scope, $http) {
-        scope.Teams = [];
+        $scope.Team = null;
+        $scope.urlGetTeam = URLS.urlGetTeam;
+        $scope.childs = [];
 
-        $scope.GetTeams = function () {
 
+        $scope.GetTeam = function () {
+            $http.get($scope.urlGetTeam)
+                .success(function (response) {
+                    $scope.Team = response;
+                    $scope.Team.lvl = 1;
+                    tree($scope.Team,$scope.Team.lvl);
+                });
         };
 
+        function tree(elem,lvl)
+        {
+         for(var i=0;i < elem.childs.length;i++)
+         {
+             elem.childs[i].lvl = lvl + 1;
+             $scope.childs.push(elem.childs[i]) ;
+            tree(elem.childs[i],elem.childs[i].lvl) ;
+         }
+        }
 
+        $scope.showChildForTeam = function(Team,lvl)
+        {
+            for(var i=0;i<$scope.childs.length;i++)
+            {
+                if($scope.childs[i].lvl == lvl + 1)
+                {
+                    $scope.childs[i].visibleelem = true;
+                }
+            }
+
+        };
         console.log('TeamController was loaded!!!')
     }
 ]);
