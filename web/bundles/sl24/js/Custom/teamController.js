c76b51bb0b9e94663c68ac01cafa7732eeeb8754
PilliVilli <andriismuzhanitsya@gmail.com>
2015-04-07 16:30:58 +0200

    teams

--- web/bundles/sl24/js/Custom/teamController.js
+++ web/bundles/sl24/js/Custom/teamController.js
@@ -10,27 +10,46 @@ Sl24.controller('TeamController', ['$scope', '$http',
                 .success(function (response) {
                     $scope.Team = response;
                     $scope.Team.lvl = 1;
-                    tree($scope.Team,$scope.Team.lvl);
+                    $scope.Team.marginrigth = 0;
+                    $scope.Team.visible = false;
+
+                    tree($scope.Team,$scope.Team.lvl,$scope.Team.marginrigth);
                 });
         };
 
-        function tree(elem,lvl)
+        function tree(elem,lvl,marginrigth)
         {
          for(var i=0;i < elem.childs.length;i++)
          {
              elem.childs[i].lvl = lvl + 1;
+             elem.childs[i].visible = false;
+             elem.childs[i].marginrigth = marginrigth + 25;
              $scope.childs.push(elem.childs[i]) ;
-            tree(elem.childs[i],elem.childs[i].lvl) ;
+            tree(elem.childs[i],elem.childs[i].lvl,elem.childs[i].marginrigth) ;
          }
         }
 
         $scope.showChildForTeam = function(Team,lvl)
         {
+            Team.visible = true;
             for(var i=0;i<$scope.childs.length;i++)
             {
                 if($scope.childs[i].lvl == lvl + 1)
                 {
                     $scope.childs[i].visibleelem = true;
+                }
+            }
+
+        };
+        $scope.removeChildForTeam = function(Team,lvl)
+        {
+            Team.visible = false;
+            for(var i=0;i<$scope.childs.length;i++)
+            {
+                if($scope.childs[i].lvl == lvl + 1)
+                {
+                    $scope.childs[i].visibleelem = false;
+                    $scope.removeChildForTeam( $scope.childs[i],$scope.childs[i].lvl);
                 }
             }
 
